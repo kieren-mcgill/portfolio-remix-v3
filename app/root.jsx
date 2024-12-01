52b176c7b41b6793123107d5127dabef5208d0bc
kacm79 <105650651+kacm79@users.noreply.github.com>
2024-12-01 08:58:14 +0000

    added sass

--- app/root.jsx
+++ app/root.jsx
@@ -4,6 +4,11 @@ import {
     Outlet,
     Scripts,
 } from "@remix-run/react";
+import styles from './styles/main.css';
+
+export const links = () => [
+    { rel: 'stylesheet', href: styles },
+];
 
 export default function App() {
     return (
